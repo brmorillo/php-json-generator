a5415f9a88c1d1af0a2f9e8e79c1f2ff6c16fd53
Bruno R. Morillo <111511828+brunormorillo@users.noreply.github.com>
2023-09-06 02:17:30 -0300

    fix:

--- index.php
+++ index.php
@@ -27,28 +27,94 @@ if (!empty($jsonOriginal)) {
 function replaceAll($jsonOriginal)
 {
     $jsonCompleto = [];
-    $replaceRepeatData = replaceRepeat($jsonOriginal, $jsonCompleto);
+    $parentIndex = 0;
+    $depth = 0;
+    $replaceRepeatData = replaceRepeat($jsonOriginal, $parentIndex, $depth);
     return $replaceRepeatData;
 }
 
-
-function replaceRepeat($jsonOriginal) {
+function replaceRepeat($jsonOriginal, $parentIndex = 0, $depth = 0) {
     $jsonCompleto = [];
+    $currentIndex = $parentIndex;
 
     foreach ($jsonOriginal as $key => $value) {
-        if (is_array($value) && isset($value['repeat()'])) {
-            $min = $value['repeat()']['options']['min'];
-            $max = $value['repeat()']['options']['max'];
+        if (is_array($value)) {
+            if (isset($value['repeat()'])) {
+                $min = $value['repeat()']['options']['min'];
+                $max = $value['repeat()']['options']['max'];
 
-            $jsonRepetido = [];
-            for($i = 1; $i <= rand($min, $max); $i++) {
-                $jsonRepetido[] = replaceRepeat($value['repeat()']['data']);  // chama recursivamente
+                $jsonRepetido = [];
+                for ($i = 1; $i <= rand($min, $max); $i++) {
+                    $jsonRepetido[] = replaceRepeat($value['repeat()']['data'], $i, $depth);
+                }
+                $jsonCompleto[$key] = $jsonRepetido;
+            } else {
+                $jsonCompleto[$key] = replaceRepeat($value, $currentIndex, $depth);
             }
-            $jsonCompleto[$key] = $jsonRepetido;
         } else {
-            $jsonCompleto[$key] = $value;
+            if ($value === "objectId()") {
+                $jsonCompleto[$key] = generateRandomHash();
+            } elseif ($value === "guid()") {
+                $jsonCompleto[$key] = generateGuid(); // adiciona suporte para guid
+            } elseif ($key === "index()" || $value === "index()") {
+                $jsonCompleto[$key] = $currentIndex + $depth - 1;
+            } elseif ($key === "bool()" || $value === "bool()") {
+                $jsonCompleto[$key] = rand(0, 1) === 1;
+            } elseif ($key === "date()" || $value === "date()") {
+                $jsonCompleto[$key] = date("Y-m-d");
+            } elseif ($key === "datetime()" || $value === "datetime()") {
+                $jsonCompleto[$key] = date("Y-m-d H:i:s");
+            } elseif ($key === "time()" || $value === "time()") {
+                $jsonCompleto[$key] = date("H:i:s");
+            } elseif ($key === "timestamp()" || $value === "timestamp()") {
+                $jsonCompleto[$key] = time();
+            } elseif ($key === "email()" || $value === "email()") {
+                $jsonCompleto[$key] = "email" . $currentIndex . "@gmail.com";
+            } elseif ($key === "name()" || $value === "name()") {
+                $jsonCompleto[$key] = "name" . $currentIndex;
+            } elseif ($key === "surname()" || $value === "surname()") {
+                $jsonCompleto[$key] = "surname" . $currentIndex;
+            } elseif ($key === "fullname()" || $value === "fullname()") {
+                $jsonCompleto[$key] = "fullname" . $currentIndex;
+            } elseif ($key === "age()" || $value === "age()") {
+                $jsonCompleto[$key] = rand(18, 80);
+            } elseif ($key === "cpf()" || $value === "cpf()") {
+                $jsonCompleto[$key] = "cpf" . $currentIndex;
+            } elseif ($key === "cnpj()" || $value === "cnpj()") {
+                $jsonCompleto[$key] = "cnpj" . $currentIndex;
+            } elseif ($key === "phone()" || $value === "phone()") {
+                $jsonCompleto[$key] = "phone" . $currentIndex;
+            } elseif ($key === "cep()" || $value === "cep()") {
+                $jsonCompleto[$key] = "cep" . $currentIndex;
+            } elseif ($key === "address()" || $value === "address()") {
+                $jsonCompleto[$key] = "address" . $currentIndex;
+            } else {
+                $jsonCompleto[$key] = $value;
+            }
         }
     }
 
     return $jsonCompleto;
+}
+
+
+function generateRandomHash()
+{
+    return md5(uniqid(rand(), true));
+    //return bin2hex(random_bytes(16)); // gera um hash aleatório
+}
+
+function generateGuid() {
+    if (function_exists('com_create_guid')) {
+        return trim(com_create_guid(), '{}');
+    } else {
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
 }
