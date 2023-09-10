79feeb2e30468881f0e276953e02e3479716943e
Bruno R. Morillo <111511828+brunormorillo@users.noreply.github.com>
2023-09-10 15:17:21 -0300

    Muda o nome do arquivo e remove teste

--- index4.php
+++
@@ -1,8 +0,0 @@
-<?php
-$a = '996609604';
-$dash = 4;
-$stringLen = strlen($a);
-$dashPosition = $stringLen - $dash;
-$b = substr($a, $dashPosition, $stringLen);
-$c = substr($a, 0, $dashPosition);
-echo $c;
